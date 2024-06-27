e09f7c591045e0a165957b200da8b57679b41feb
Thomas Clowes <thomas@doublenegative.com>
2024-06-27 11:39:41 +0100

    - Concise imports in example files

--- v2/test/examples/ENSv2/storage.ts
+++ v2/test/examples/ENSv2/storage.ts
@@ -1,12 +1,14 @@
-import {ethers} from 'ethers';
+import {dnsEncode, namehash, ZeroAddress, ZeroHash, AbiCoder, toUtf8String} from 'ethers';
 import {Foundry} from '@adraffy/blocksmith';
 import {EVMProver, EVMRequest} from '../../../src/vm.js';
 import { HexString } from '../../../src/types.js';
 
+const coder = AbiCoder.defaultAbiCoder();
+
 let foundry = await Foundry.launch({procLog: false});
 
 function dns(name: string) {
-	return ethers.dnsEncode(name, 255);
+	return dnsEncode(name, 255);
 }
 
 async function deployResolver() {
@@ -32,16 +34,16 @@ let storage = await foundry.deploy({file: 'RegistryStorage'});
 async function deployRegistry(parent: HexString) {
 	return foundry.deploy({file: 'V2Registry', args: [storage, parent]});
 }
-let registry_root = await deployRegistry(ethers.ZeroAddress);
+let registry_root = await deployRegistry(ZeroAddress);
 let registry_eth  = await deployRegistry(registry_root.target);
 
-await foundry.confirm(storage.setRegistry(ethers.ZeroAddress, ethers.ZeroHash, registry_root))
+await foundry.confirm(storage.setRegistry(ZeroAddress, ZeroHash, registry_root))
 await foundry.confirm(registry_root.setSubnode(wallet_admin, dns('eth'), resolver_eth, registry_eth, wallet_admin));
-await foundry.confirm(registry_eth.setSubnode(wallet_raffy, dns('raffy.eth'), resolver_raffy, ethers.ZeroAddress, wallet_raffy));
+await foundry.confirm(registry_eth.setSubnode(wallet_raffy, dns('raffy.eth'), resolver_raffy, ZeroAddress, wallet_raffy));
 
-await foundry.confirm(resolver_eth.setText(ethers.namehash('chonk.eth'), 'name', 'Chonk'));
-await foundry.confirm(resolver_raffy.setText(ethers.namehash('raffy.eth'), 'name', 'Raffy'));
-await foundry.confirm(resolver_raffy.setText(ethers.namehash('sub.raffy.eth'), 'name', 'Subdomain!'));
+await foundry.confirm(resolver_eth.setText(namehash('chonk.eth'), 'name', 'Chonk'));
+await foundry.confirm(resolver_raffy.setText(namehash('raffy.eth'), 'name', 'Raffy'));
+await foundry.confirm(resolver_raffy.setText(namehash('sub.raffy.eth'), 'name', 'Subdomain!'));
 
 let prover = await EVMProver.latest(foundry.provider);
 //prover.log = console.log;
@@ -51,7 +53,7 @@ async function resolve(name: string) {
 	let req = new EVMRequest(3);
 	req.setTarget(storage.target); // use storage contract
 	req.push(0).setOutput(0); // start at root (not actually needed)
-	name.split('.').forEach((_, i, v) => req.push(ethers.namehash(v.slice(i).join('.'))));
+	name.split('.').forEach((_, i, v) => req.push(namehash(v.slice(i).join('.'))));
 	req.push(0); // add namehash for root
 	req.setSlot(0) // _nodes mapping
 	req.begin()
@@ -67,7 +69,7 @@ async function resolve(name: string) {
 	.end().eval({failure: true}) // loop until we get a failure
 	req.pushOutput(1).requireNonzero().target() // set target to resolver
 		.setSlot(0) // _texts mapping
-		.push(ethers.namehash(name)).follow().pushStr('name').follow() // _texts[node][key]
+		.push(namehash(name)).follow().pushStr('name').follow() // _texts[node][key]
 		.readBytes().setOutput(2); // read text(name) store into output	
 
 	let state = await prover.evalRequest(req);
@@ -75,14 +77,14 @@ async function resolve(name: string) {
 	//console.log(state);
 	console.log({
 		name,
-		registry: ethers.AbiCoder.defaultAbiCoder().decode(['address'], values[0])[0],
+		registry: coder.decode(['address'], values[0])[0],
 	});
 	if (state.exitCode) {
 		console.log(`<doesn't exist>`);
 	} else {
 		console.log({
-			resolver: ethers.AbiCoder.defaultAbiCoder().decode(['address'], values[1])[0],
-			text: ethers.toUtf8String(values[2])
+			resolver: coder.decode(['address'], values[1])[0],
+			text: toUtf8String(values[2])
 		});
 	}
 }
--- v2/test/examples/TeamNick/fetch.ts
+++ v2/test/examples/TeamNick/fetch.ts
@@ -1,9 +1,11 @@
 import {EVMProver, EVMRequest} from '../../../src/vm.js';
 import {CHAIN_BASE, createProvider} from '../../../src/providers.js';
-import {ethers} from 'ethers';
+import {AbiCoder, toUtf8String} from 'ethers';
 
+const coder = AbiCoder.defaultAbiCoder();
 let prover = await EVMProver.latest(createProvider(CHAIN_BASE));
 
+//https://basescan.org/address/0x7C6EfCb602BC88794390A0d74c75ad2f1249A17f#code
 let req = new EVMRequest(3).setTarget('0x7C6EfCb602BC88794390A0d74c75ad2f1249A17f');
 req.setSlot(8).read().setOutput(0);
 req.setSlot(7).pushStr("raffy").keccak().follow().read().setOutput(1);
@@ -16,8 +18,8 @@ console.log(state);
 let values = await state.resolveOutputs();
 
 console.log({
-	supply: ethers.AbiCoder.defaultAbiCoder().decode(['uint256'], values[0])[0],
-	address: ethers.AbiCoder.defaultAbiCoder().decode(['address'], values[1])[0],
-	avatar: ethers.toUtf8String(values[2]),
+	supply: coder.decode(['uint256'], values[0])[0],
+	address: coder.decode(['address'], values[1])[0],
+	avatar: toUtf8String(values[2]),
 });
 
