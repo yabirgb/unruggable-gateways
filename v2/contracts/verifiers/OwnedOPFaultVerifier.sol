ccaaebd89260c833b7673bf60372f7085ef0cac7
Thomas Clowes <thomas@doublenegative.com>
2024-06-30 13:53:43 +0100

    - Added example/tests for a Solidity implementation of the proposed v2 architecture - Port config for gateway operation

---
+++ v2/contracts/verifiers/OwnedOPFaultVerifier.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "../EVMProtocol.sol";
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+import {IEVMVerifier} from "../IEVMVerifier.sol";
+import {EVMProver, ProofSequence} from "../EVMProver.sol";
+import {MerkleTrieHelper} from "../MerkleTrieHelper.sol";
+
+import {RLPReader} from "@eth-optimism/contracts-bedrock/src/libraries/rlp/RLPReader.sol";
+import {Hashing, Types} from "@eth-optimism/contracts-bedrock/src/libraries/Hashing.sol";
+import "@eth-optimism/contracts-bedrock/src/dispute/interfaces/IDisputeGameFactory.sol";
+
+interface IOptimismPortal {
+	function disputeGameFactory() external view returns (IDisputeGameFactory);
+	function respectedGameType() external view returns (GameType);
+}
+
+contract OwnedOPFaultVerifier is IEVMVerifier, Ownable {
+
+	event GatewayConfigChanged(string[] urls, uint256 delay);
+
+	uint256 constant GAME_CHUNK = 10;
+
+	string[] _gatewayURLs;
+	IOptimismPortal immutable _portal;
+	uint256 _delay;
+
+	constructor(string[] memory urls, IOptimismPortal portal, uint256 delay) Ownable(msg.sender) {
+		_portal = portal;
+        setGatewayConfig(urls, delay);
+	}
+
+    function setGatewayConfig(string[] memory urls, uint256 delay) onlyOwner public {
+		_gatewayURLs = urls;
+		_delay = delay;
+		emit GatewayConfigChanged(urls, delay);
+	}
+
+	function gatewayURLs() external view returns (string[] memory) {
+		return _gatewayURLs;
+	}
+	function getLatestContext() external view returns (bytes memory) {
+		return abi.encode(findLatestGame());
+	}
+
+	function findLatestGame() internal view returns (uint256) {
+		IDisputeGameFactory factory = _portal.disputeGameFactory();
+		GameType rgt = _portal.respectedGameType();
+		uint256 n = factory.gameCount();
+		n = n > _delay ? n - _delay : 0;
+		while (n > 0) {
+			uint256 c = n > GAME_CHUNK ? GAME_CHUNK : n;
+			IDisputeGameFactory.GameSearchResult[] memory gs = factory.findLatestGames(rgt, n - 1, c);
+			n -= c;
+			for (uint256 i; i < gs.length; i++) {
+				(, , address gameProxy) = gs[i].metadata.unpack();
+				if (IDisputeGame(gameProxy).status() != GameStatus.CHALLENGER_WINS) {
+					return gs[i].index;
+				}
+			}
+		}
+		revert VerifierUnsatisfiable();
+	}
+
+	function getStorageValues(bytes memory context, EVMRequest memory req, bytes memory proof) external view returns (bytes[] memory, uint8 exitCode) {
+		uint256 gameIndex = abi.decode(context, (uint256));
+		(
+			Types.OutputRootProof memory outputRootProof, 
+			bytes[][] memory proofs,
+			bytes memory order
+		) = abi.decode(proof, (Types.OutputRootProof, bytes[][], bytes));
+		(, , IDisputeGame gameProxy) = _portal.disputeGameFactory().gameAtIndex(gameIndex);
+		bytes32 outputRoot = gameProxy.rootClaim().raw();
+		bytes32 expectedRoot = Hashing.hashOutputRootProof(outputRootProof);
+		if (outputRoot != expectedRoot) {
+			revert VerifierMismatch(context, expectedRoot, outputRoot);
+		}
+		return EVMProver.evalRequest(req, ProofSequence(0, outputRootProof.stateRoot, proofs, order, MerkleTrieHelper.proveAccountState, MerkleTrieHelper.proveStorageValue));
+	}
+
+}
---
+++ v2/test/examples/ENSv2/resolver.test.ts
@@ -0,0 +1,168 @@
+import { Foundry } from '@adraffy/blocksmith';
+import { serve } from '@resolverworks/ezccip';
+import {
+  createProvider,
+  providerURL,
+  CHAIN_OP,
+} from '../../../src/providers.js';
+import { OPFaultGateway } from '../../../src/gateway/OPFaultGateway.js';
+import {
+  dnsEncode,
+  AbiCoder,
+  toBeHex,
+  namehash,
+  getAddress,
+  Interface,
+} from 'ethers';
+import { solidityFollowSlot } from '../../../src/vm.js';
+import { afterAll, describe, test, expect } from 'bun:test';
+
+export const ABI_CODER = AbiCoder.defaultAbiCoder();
+
+function dns(name) {
+  return dnsEncode(name, 255);
+}
+
+const ENS = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
+const NAME = 'clowes.eth';
+const NODE = namehash(NAME);
+const SLOT = solidityFollowSlot(0, NODE) + 1n;
+
+const MY_ADDRESS = '0xAC50cE326de14dDF9b7e9611Cd2F33a1Af8aC039';
+
+describe('ENSv2', async () => {
+  const foundry = await Foundry.launch({
+    fork: providerURL(1),
+    infoLog: true,
+    procLog: true,
+  });
+
+  afterAll(() => foundry.shutdown());
+
+  /**
+   * To run the example against a development gateway, uncomment the below
+   * and comment out the hardcoded values that follow.
+   */
+
+  /*
+  let gateway = OPFaultGateway.mainnet({
+    provider1: foundry.provider,
+    provider2: createProvider(CHAIN_OP),
+    commitDelay: 0,
+  });
+
+  afterAll(() => gateway.shutdown());
+
+  let ccip = await serve(gateway, { protocol: 'raw' });
+
+  afterAll(() => ccip.shutdown());
+
+  const gatewayUrls = [ccip.endpoint];
+  const optimismPortal = gateway.OptimismPortal;
+  const delay = gateway.commitDelay;
+  */
+
+  const gatewayUrls = ['https://op-gateway.unruggable.com'];
+  const optimismPortal = '0xbEb5Fc579115071764c7423A4f12eDde41f106Ed';
+  const delay = 0;
+
+  const verifier = await foundry.deploy({
+    file: 'OwnedOPFaultVerifier',
+    args: [gatewayUrls, optimismPortal, delay],
+  });
+
+  const opResolver = await foundry.deploy({
+    file: 'contracts/OPResolver',
+    args: [verifier],
+  });
+
+  const resolverBefore = await foundry.provider.getResolver(NAME);
+  console.log('Queried before: ', resolverBefore?.address);
+
+  // replace real resolver with fake (the one we just deployed)
+  await foundry.provider.send('anvil_setStorageAt', [
+    ENS,
+    toBeHex(SLOT, 32),
+    toBeHex(opResolver.target, 32),
+  ]);
+
+  //Get the Resolver using ethers
+  const resolver = await foundry.provider.getResolver(NAME);
+
+  test('verify resolver was hijacked', async () => {
+    expect(resolver?.address).toBe(opResolver.target);
+  });
+
+  test('get address basic', async () => {
+    //Resolve the name using ethers in built resolution
+    const coinTypeAddress = await resolver?.getAddress();
+    expect(coinTypeAddress).toBe(MY_ADDRESS);
+  });
+
+  test('get address w/ cointype', async () => {
+    //Resolve the name using ethers in built resolution
+    const coinTypeAddress = await resolver?.getAddress(1);
+    expect(coinTypeAddress).toBe(MY_ADDRESS);
+  });
+
+  test('manually constructed basic resolution call', async () => {
+    //Manually construct the IAddrResolver calldata for an extra check
+    const addrInterface = new Interface([
+      'function addr(bytes32 node) returns (address payable)',
+    ]);
+
+    const resolutionCalldata = addrInterface.encodeFunctionData('addr', [NODE]);
+
+    //Manually call the resolution function
+    const result = await opResolver.resolve(dns(NAME), resolutionCalldata, {
+      enableCcipRead: true,
+    });
+
+    //Decode the result
+    const resolvedAddress = ABI_CODER.decode(['address'], result)[0];
+    console.log('Resolved address (manual): ', resolvedAddress);
+
+    expect(resolvedAddress).toBe(MY_ADDRESS);
+  });
+
+  test.only('manually constructed resolution call w/cointype', async () => {
+    //Manually construct the IAddressResolver calldata for an extra check
+    const addressInterface = new Interface([
+      'function addr(bytes32 node, uint256 coinType) returns (bytes memory)',
+    ]);
+
+    const resolutionCalldata = addressInterface.encodeFunctionData('addr', [
+      NODE,
+      1,
+    ]);
+
+    //Manually call the resolution function
+    const result = await opResolver.resolve(dns(NAME), resolutionCalldata, {
+      enableCcipRead: true,
+    });
+
+    //Decode the result
+    const resolvedAddress = getAddress(ABI_CODER.decode(['bytes'], result)[0]);
+    console.log('Resolved address (manual): ', resolvedAddress);
+
+    expect(resolvedAddress).toBe(MY_ADDRESS);
+  });
+
+  /**
+   * The below outputs all three addresses if utilising the debugCallback in OPResolver.sol
+   */
+
+  /*
+  const [bytesArray] = ABI_CODER.decode(['bytes[]'], result);
+
+  console.log('bytesArray', bytesArray);
+
+  const [registry, resolver, resolved] = bytesArray.map(
+    (value) => ABI_CODER.decode(['address'], value)[0]
+  );
+
+  console.log('Registry: ', registry);
+  console.log('Resolver: ', resolver);
+  console.log('Resolved: ', resolved);
+  */
+});
