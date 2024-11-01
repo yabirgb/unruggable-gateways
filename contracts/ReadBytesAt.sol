7039259ebb78099a383ba9f14de5599eec71e71a
Thomas Clowes <thomas@doublenegative.com>
2024-11-01 09:46:55 +0000

    - minimal changes

--- contracts/ReadBytesAt.sol
+++ contracts/ReadBytesAt.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract ReadBytesAt {
-    // NOTE: pure seems like the wrong mutability annotation
+    // NOTE: pure is the wrong mutability annotation but because of the direct use of inline assembly to set v.slot, Solidity can’t verify that it’s only reading storage and not modifying it
     function readBytesAt(uint256 slot) external pure returns (bytes memory) {
         bytes storage v;
         assembly {
