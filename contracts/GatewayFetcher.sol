34ec13f9657a4c5769b828d62dbc219799f4f167
Thomas Clowes <thomas@doublenegative.com>
2024-12-04 14:19:50 +0000

    - Correctly validate that MAX_OPS is not exceeded in addBytes function

--- contracts/GatewayFetcher.sol
+++ contracts/GatewayFetcher.sol
@@ -52,7 +52,7 @@ library GatewayFetcher {
         bytes memory v
     ) internal pure returns (GatewayRequest memory) {
         bytes memory buf = r.ops;
-        if (r.ops.length + v.length >= MAX_OPS) revert RequestOverflow();
+        if (r.ops.length + v.length > MAX_OPS) revert RequestOverflow();
         assembly {
             let dst := add(add(buf, 32), mload(buf)) // ptr to write
             let src := add(v, 32) // ptr to start read
